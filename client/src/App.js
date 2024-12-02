53e32c8395c40b5634f7ebc5a3e409d1a6cd5634
Harsh Sharma <hs804506@gmail.com>
2024-12-02 18:20:08 +0530

    socket connection established with frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { SocketProvider } from './providers/Socket';
+import { Socket } from 'socket.io-client';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/world" element={<h1>World !!</h1>} />
+        </Routes>
+      </SocketProvider>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,13 +2,27 @@ const express = require("express");
 const { Server } = require("socket.io");
 const bodyParser = require("body-parser");
 
-const io = new Server();
+const io = new Server({
+    cors: true
+});
 const app = express();
 
 app.use(bodyParser.json());
 
-io.on("connection", (socket) => {
+const emailToSocketMapping = new Map();
 
+io.on("connection", (socket) => {
+    socket.on("join-room", data => {
+        const { roomId, emailId } = data;
+        console.log(roomId, emailId);
+        emailToSocketMapping.set(emailId, socket.id);
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit("user-joined", { emailId });
+    })
 })
 
 app.listen(8000, () => console.log("HTTP server is running on PORT 8000!"))
+io.listen(8001);
+
+
+
