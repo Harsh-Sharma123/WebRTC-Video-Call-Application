0b53d19dead52fd7da40d820a6eb20d8337002a0
Harsh Sharma <hs804506@gmail.com>
2024-12-03 22:43:16 +0530

    checking if someone enters same room

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import {Routes, Route} from 'react-router-dom';
 import { Home } from './pages/Home';
 import { SocketProvider } from './providers/Socket';
 import { Socket } from 'socket.io-client';
+import { RoomPage } from './pages/Room';
+import { PeerProvider } from './providers/Peer';
 
 function App() {
   return (
     <div className="App">
       <SocketProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/world" element={<h1>World !!</h1>} />
-        </Routes>
+        <PeerProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/:roomid" element={<RoomPage />} />
+          </Routes>
+        </PeerProvider>
       </SocketProvider>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSocket } from '../providers/Socket'
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
 
-  const { socket }= useSocket();
-  socket.emit('join-room', {roomId: "1", emailId: "hs"})
+  const navigate = useNavigate();
+
+  const { socket } = useSocket();
+
+  const [roomid, setRoomId] = useState();
+  const [emailid, setEmailId] = useState();
+
+  const handleRoomJoin = () => {
+    socket.emit('join-room', { roomId: roomid, emailId: emailid})
+  }
+
+  const handleRoomJoined = ({ roomId }) => {
+    console.log(roomId);
+    navigate(`/room/${roomId}`)
+  }
+
+  useEffect(() => {
+    socket.on("joined-room", handleRoomJoined);
+  }, [socket]);
 
   return (
     <div className='homepage-container'>
       <div className='input-container'>
-        <input type="email" placeholder='Enter your email here' />
-        <input type="text" placeholder="Enter your room code" />
-        <button type="submit">Enter Room</button>
+        <input type="email" placeholder='Enter your email here' onChange={(e)=>setEmailId(e.target.value)} />
+        <input type="text" placeholder="Enter your room code" onChange={(e) => setRoomId(e.target.value)} />
+        <button type="submit" onClick={handleRoomJoin}>Enter Room</button>
       </div>
     </div>
   )
